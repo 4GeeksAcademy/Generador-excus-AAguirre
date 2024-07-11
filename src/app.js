95257cb6b5df9b59e46990243c0ab5b747e27b72
aaguirre <alejandra36a@gmail.com>
2024-07-11 11:23:40 +0000

    Generador de excusas final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#bot").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuses();
+  });
+  console.log("Hello");
+};
+let generateExcuses = () => {
+  let who = ["She", "He", "The dog", "Barbie"];
+  let action = ["run", "sleep", "eat", "cry", "swin"];
+  let what = ["the book", "the pizza", "career"];
+  let when = [
+    "before the class",
+    "when I was wake up",
+    "while I was exercising",
+    "during my breakfast",
+    "while I was praying"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
